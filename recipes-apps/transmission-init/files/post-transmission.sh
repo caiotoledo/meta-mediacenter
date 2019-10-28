077424bdb4542633dbf322cdb23d49e44c46fdc9
Caio Toledo <caioviniciusdetoledo@gmail.com>
2019-10-28 00:07:54 +0000

    Fix script for paths with spaces
    Signed-off-by: Caio Toledo <caioviniciusdetoledo@gmail.com>

--- recipes-apps/transmission-init/files/post-transmission.sh
+++ recipes-apps/transmission-init/files/post-transmission.sh
@@ -4,5 +4,5 @@ LOG_FILE=/var/log/post-transmission.log
 TORRENT_FOLDER="${TR_TORRENT_DIR}/${TR_TORRENT_NAME}"
 
 echo "Starting at $(date): ${TORRENT_FOLDER}" >> ${LOG_FILE}
-find ${TORRENT_FOLDER} -type f | xargs -I{} /etc/transmission/subtitle-find.sh {} >> ${LOG_FILE}
+find "${TORRENT_FOLDER}" -type f | xargs -I{} /etc/transmission/subtitle-find.sh "{}" >> ${LOG_FILE}
 echo "Finished at $(date): ${TORRENT_FOLDER}" >> ${LOG_FILE}
--- recipes-apps/transmission-init/files/subtitle-find.sh
+++ recipes-apps/transmission-init/files/subtitle-find.sh
@@ -5,7 +5,7 @@ get_ffmpeg_info() {
 }
 
 is_video(){
-	info=$(get_ffmpeg_info $1 | grep 'Metadata')
+	info=$(get_ffmpeg_info "$1" | grep 'Metadata')
 	if [ -n "$info" ]; then
 		echo 1
 	else
@@ -27,14 +27,14 @@ if [ -z "$LANG" ]; then
 	LANG="pt-br"
 fi
 
-isVideo=$(is_video $FILE)
+isVideo=$(is_video "$FILE")
 if [ $isVideo == 1 ]
 then
 	# Install subliminal if needed
 	pip install subliminal
 	echo "Searching a subtitle for ${FILE}"
 	# Download the Subtitle
-	subliminal download -s -l $LANG $FILE
+	subliminal download -s -l "$LANG" "$FILE"
 else
 	echo "${FILE} Not a video file!"
 	exit 1
