cd28bca0c6b28d2ba7debe72a2fd13d3f2ef79a4
Caio Toledo <caioviniciusdetoledo@gmail.com>
2021-03-16 00:11:42 +0000

    transmission-init: Output errors to log file
    Signed-off-by: Caio Toledo <caioviniciusdetoledo@gmail.com>

--- recipes-apps/transmission-init/files/post-transmission.sh
+++ recipes-apps/transmission-init/files/post-transmission.sh
@@ -7,7 +7,7 @@ TORRENT_FOLDER="${TR_TORRENT_DIR}/${TR_TORRENT_NAME}"
 OPENSUBTITLES_USERNAME="%OPENSUBTITLES_USERNAME%"
 OPENSUBTITLES_PASSWORD="%OPENSUBTITLES_PASSWORD%"
 
-echo "Starting at $(date): ${TORRENT_FOLDER}" >> ${LOG_FILE}
-find "${TORRENT_FOLDER}" -type f | xargs -I{} /etc/transmission/subtitle-find.sh "{}" "${OPENSUBTITLES_USERNAME}" "${OPENSUBTITLES_PASSWORD}" >> ${LOG_FILE}
-find "${TORRENT_FOLDER}" -type f | xargs -I{} /etc/transmission/convert2ac3.sh "{}" >> ${LOG_FILE}
-echo "Finished at $(date): ${TORRENT_FOLDER}" >> ${LOG_FILE}
+echo "Starting at $(date): ${TORRENT_FOLDER}" &>> ${LOG_FILE}
+find "${TORRENT_FOLDER}" -type f | xargs -I{} /etc/transmission/subtitle-find.sh "{}" "${OPENSUBTITLES_USERNAME}" "${OPENSUBTITLES_PASSWORD}" &>> ${LOG_FILE}
+find "${TORRENT_FOLDER}" -type f | xargs -I{} /etc/transmission/convert2ac3.sh "{}" &>> ${LOG_FILE}
+echo "Finished at $(date): ${TORRENT_FOLDER}" &>> ${LOG_FILE}
